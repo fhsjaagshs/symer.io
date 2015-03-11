42b18a55f7a462e548c3ba42be68f52f48d49bcf
Nathaniel Symer <nate@natesymer.com>
2015-03-11 16:34:56 -0400

    Finish init script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,13 @@ tar czfv blog.deploy blog assets migrations
 rm blog
 ssh nathaniel@blog.symer.io "mkdir -p /deploy/blog.symer.io.new"
 scp blog.deploy nathaniel@blog.symer.io:/deploy/blog.symer.io.new/
-ssh nathaniel@blog.symer.io "cd /deploy/blog.symer.io.new; tar zxvf blog.deploy; rm blog.deploy; cd ..; mv blog.symer.io blog.symer.io.old; mv blog.symer.io.new blog.symer.io; sudo /etc/init.d/blog restart;"
-
-
+ssh nathaniel@blog.symer.io <<EOT
+cd /deploy/blog.symer.io.new;
+tar zxvf blog.deploy;
+rm blog.deploy;
+cd /deploy;
+mv blog.symer.io blog.symer.io.old;
+mv blog.symer.io.new blog.symer.io;
+chmod +x /deploy/blog.symer.io/blog
+/etc/init.d/blog restart;
+EOT
