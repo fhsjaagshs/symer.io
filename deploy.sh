cfacdba5d7585c6679eb98feced22d47074de275
Nathaniel Symer <nate@natesymer.com>
2015-03-13 11:27:23 -0400

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,14 @@
 cabal build
 cp dist/build/blog/blog blog
 strip blog
-tar czfv blog.deploy blog assets migrations
-rm blog
-ssh nathaniel@blog.symer.io "mkdir -p /deploy/blog.symer.io.new"
-scp blog.deploy nathaniel@blog.symer.io:/deploy/blog.symer.io.new/
-ssh nathaniel@blog.symer.io <<EOT
-cd /deploy/blog.symer.io.new;
-tar zxvf blog.deploy;
-rm blog.deploy;
+tar czfv - blog assets migrations | ssh nathaniel@blog.symer.io <<EOSSH
+mkdir -p /deploy/blog.symer.io.new
+cd /deploy/blog.symer.io.new
+tar zxvf -
+chmod +x blog
 cd /deploy;
 mv blog.symer.io blog.symer.io.old;
 mv blog.symer.io.new blog.symer.io;
-chmod +x /deploy/blog.symer.io/blog
 /etc/init.d/blog try-restart;
-EOT
+EOSSH
+rm blog
