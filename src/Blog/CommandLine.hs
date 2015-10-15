6db010b7b728d995fd38e91b5fd41fce57b78c1b
Nathaniel Symer <nate@natesymer.com>
2015-10-15 15:07:02 -0400

    Improve commandline parsing

--- src/Blog/CommandLine.hs
+++ src/Blog/CommandLine.hs
@@ -6,11 +6,11 @@ module Blog.CommandLine
 )
 where
 
-import qualified System.Console.Terminal.Size as TerminalSize -- for optparse-applicative print width
+import Blog.Terminal
 
 import Control.Applicative
 import Options.Applicative
-import System.Environment
+import System.Environment (getArgs)
 
 data Cmd
   = StartCommand {
@@ -26,55 +26,38 @@ data Cmd
   | StopCommand
   | StatusCommand
 
--- options
+getCommand :: IO Cmd
+getCommand = getArgs >>= getCommandArgs
 
-mkcmd :: String -> String -> Parser a ->  Mod CommandFields a
-mkcmd cmd desc p = command cmd $ info (helper <*> p) $ progDesc desc
-
-parser :: ParserInfo Cmd
-parser = info (helper <*> parseCommand) (fullDesc <> progDesc "Nathaniel Symer's blog."
-                                                  <> (header $ "blog.symer.io")) 
-
+getCommandArgs :: [String] -> IO Cmd
+getCommandArgs [] = getCommandArgs ["start"]
+getCommandArgs args = do
+  mts <- getTermSize
+  handleParseResult $ execParserPure (pprefs $ width mts) parser args
+  where
+    pprefs = ParserPrefs "" False False True
+    width (Just (w, _)) = w
+    width _ = 80
+    parser = info (helper <*> parseCommand) (fullDesc <> progDesc "Nathaniel Symer's blog." <> header "blog.symer.io")
+    
 parseCommand :: Parser Cmd
 parseCommand = sp <|> parseStart
   where
     sp = subparser ((mkcmd "start" "Start the blog" parseStart) <>
                     (mkcmd "stop" "Stop the blog" parseStop) <>
                     (mkcmd "status" "Determine if the blog is running" parseStatus) <>
-                    (mkcmd "redirect-http" "Redirect HTTP to HTTPs" parseRedirect)
-                    )
-    parseStart :: Parser Cmd
+                    (mkcmd "redirect-http" "Redirect HTTP to HTTPs" parseRedirect))
     parseStart = StartCommand
-      <$> (flag False True (short 'd'))
-      <*> (option auto (long "port" <> short 'p' <> metavar "PORT" <> value 3000 <> help "The port to run blog on."))
-      <*> (strOption (long "dbpasswd" <> short 'l' <> metavar "PASSWORD" <> value "" <> help "The password used to connect to Postgres."))
-      <*> (strOption (long "crtfile" <> short 'c' <> metavar "FILEPATH" <> value "server.crt" <> help "The SSL .crt file used for SSL."))
-      <*> (strOption (long "keyfile" <> short 'k' <> metavar "FILEPATH" <> value "server.key" <> help "The SSL .key file used for SSL."))
-      <*> (optional $ strOption (long "stdout" <> short 'o' <> metavar "FILEPATH" <> help "Where to redirect STDOUT to. Has no effect unless daemonized."))
-      <*> (optional $ strOption (long "stderr" <> short 'e' <> metavar "FILEPATH" <> help "Where to redirect STDERR to. Has no effect unless daemonized."))
-    parseStop :: Parser Cmd
-    parseStop = pure $ StopCommand
-    parseStatus :: Parser Cmd
-    parseStatus = pure $ StatusCommand
-    parseRedirect :: Parser Cmd
+      <$> (flag False True $ short 'd')
+      <*> (option auto $ opt "port" 'p' "PORT" (Just 3000) "The port to run blog on.")
+      <*> (strOption $ opt "dbpasswd" 'l' "PASSWORD" (Just "") "The password used to connect to Postgres.")
+      <*> (strOption $ opt "keyfile" 'k' "FILEPATH" (Just "server.key") "The SSL .key file used for SSL.")
+      <*> (strOption $ opt "crtfile" 'c' "FILEPATH" (Just "server.crt") "The SSL .crt file used for SSL.")
+      <*> (optional $ strOption $ opt "stdout" 'o' "FILEPATH" Nothing "Redirect STDOUT to a file.")
+      <*> (optional $ strOption $ opt "stderr" 'e' "FILEPATH" Nothing "Redirect STDERR to a file.")
+    parseStop     = pure $ StopCommand
+    parseStatus   = pure $ StatusCommand
     parseRedirect = pure $ RedirectCommand
-
--- optparse-applicative Helpers
-
-termWidth :: IO Int
-termWidth = termWidth' <$> TerminalSize.size
-  where
-    termWidth' (Just (TerminalSize.Window _ w)) = w :: Int
-    termWidth' _ = 10
-
-parserPrefs :: Int -> ParserPrefs
-parserPrefs width = ParserPrefs "" False False True width
-
-getCommand :: IO Cmd
-getCommand = getArgs >>= getCommandArgs
-
-getCommandArgs :: [String] -> IO Cmd
-getCommandArgs [] = getCommandArgs ["serve", "."]
-getCommandArgs args = do
-  ps <- parserPrefs <$> termWidth
-  handleParseResult $ execParserPure ps parser args
+    opt lng shrt mvar (Just defVal) hlp = (long lng <> short shrt <> metavar mvar <> value defVal <> help hlp)
+    opt lng shrt mvar Nothing       hlp = (long lng <> short shrt <> metavar mvar <> help hlp)
+    mkcmd cmd desc p = command cmd $ info (helper <*> p) $ progDesc desc
--- src/Blog/MarkdownUtil.hs
+++ src/Blog/MarkdownUtil.hs
@@ -12,13 +12,12 @@ where
 
   stripMarkdown -> removes all markdown formatting from a markdown document
   truncateMarkdown -> truncates markdown based on length of rendered text
-
 -}
 
 import Cheapskate
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Sequence (Seq)
+import Data.Sequence (Seq, (|>))
 import qualified Data.Sequence as S
 import qualified Data.Foldable as F
 
@@ -63,10 +62,11 @@ truncateMarkdown len (Doc o bs) = Doc o $ loop len (F.toList bs) S.empty
     lenBlock (CodeBlock _ code) = T.length code
     lenBlock (HtmlBlock htmlText) = T.length htmlText
     lenBlock HRule = 0
-    
+
+    -- FIXME: doesn't match all cases
     loop :: Int -> [Block] -> Seq Block -> Seq Block
-    loop _ [] accum = accum
-    loop 0 _ accum = accum
+    loop _    []     accum = accum
+    loop 0    _      accum = accum
     loop rlen (x:xs) accum
-      | lenBlock x < rlen = loop (rlen-(lenBlock x)) xs (accum S.|> x)
-      | lenBlock x > rlen = accum S.|> x
+      | lenBlock x < rlen = loop (rlen-(lenBlock x)) xs (accum |> x)
+      | lenBlock x > rlen = accum |> x
